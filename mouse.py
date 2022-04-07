79ec431f77e89816b35ae29cc9a492c12f56de91
FelipeIzui <felipegustavoizui@gmail.com>
2022-04-06 21:04:49 -0300

    Fix coordinate bug

--- mouse.py
+++ mouse.py
@@ -1,7 +1,6 @@
 #pip install pyautogui
 #pip install psutil
 
-from cProfile import run
 import pyautogui
 import psutil
 from random import randint
@@ -9,28 +8,29 @@ import time
 import os
 import sys
 
-while(True):
+while True:
     valor = 0
 
     for a in range(1920):
         x = randint(0, 1920)
-        for b in range(1080):
-            y = randint(0, 1080)
-            pyautogui.moveTo(x, y, duration = 1)
-            valor = valor + 1
-            if valor == 5:
-                click = randint(1, 3)
-                if click == 1:
-                    pyautogui.leftClick(x, y)
-                if click == 2:
-                    pyautogui.middleClick(x, y)
-                if click == 3:
-                    pyautogui.rightClick(x, y)
-                    time.sleep(2)
-                    pyautogui.rightClick(x, y)
-                print(f'cliquei nas coordenadas: {x}:{y}')
-                valor = 0
-            time.sleep(1)
-    time.sleep(60)
+        y = randint(120, 1032)
+        pyautogui.moveTo(x, y, duration = 1)
+        valor = valor + 1
+        if valor == 5:
+            click = randint(1, 3)
+            if click == 1:
+                pyautogui.leftClick(x, y)
+                print(f'Left click on coords: {x} : {y}')
+            if click == 2:
+                pyautogui.middleClick(x, y)
+                print(f'Right click on coords: {x} : {y}')
+            if click == 3:
+                pyautogui.rightClick(x, y)
+                time.sleep(2)
+                pyautogui.rightClick(x, y)
+                print(f'Middle click on coords: {x} : {y}')
+            valor = 0
+        time.sleep(5)
+time.sleep(60)
 
 
